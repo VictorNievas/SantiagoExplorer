bd09c27bcf01eb27e8ea6d4d22e2363e6c2de6f1
VictorNievas17 <victor.nievas@ansotec.com>
2025-07-20 19:54:17 +0200

    Anadido build y dist

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,6 @@ import LandingPage from "./landingpage/landingpage.jsx";
 import { useEffect } from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-import { AdMob } from '@capacitor-community/admob';
 
 function App() {
   const usuario = localStorage.getItem("usuario");
--- frontend/src/navbar/navbar.jsx
+++ frontend/src/navbar/navbar.jsx
@@ -2,7 +2,6 @@
 
 import { useState, useEffect } from "react"
 import { Link } from "react-router-dom"
-import { AdMob } from '@capacitor-community/admob'
 
 
 const apiURL = process.env.REACT_APP_API_URL
